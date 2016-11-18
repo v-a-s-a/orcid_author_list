f8328a2336a54c7999745f07e0ec4ca6f6ae6001
vasya <trubetskoy.vasa@gmail.com>
2016-11-18 16:52:35 +0100

    Started thinking about how to parse Cathryn's changes

--- src/utilities.py
+++ src/utilities.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
+import re
 
-def remove_duplicates(li):
+
+def remove_duplicates(l):
     my_set = set()
     res = []
-    for e in li:
+    for e in l:
         if e not in my_set:
             res.append(e)
             my_set.add(e)
+    return res
+
+
+def munge_field(arg):
+    """
+    Take a naming field and remove extra white spaces.
+    """
+    if arg:
+        res = re.sub(r'\s+', r' ', arg).strip()
+    else:
+        res = ''
     return res
